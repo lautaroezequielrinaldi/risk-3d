f0b402d3b2781219a3a05c870a57ee464b5de288
carlos.pantelides <carlos.pantelides@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-05-12 23:35:09 +0000

    pseudocodigos

---
+++ source/client/editor/main.cpp
@@ -0,0 +1,16 @@
+int main (int argc, char ** argv){
+	// estados
+	// nada -> click en un lugar virgen -> crea ciudad
+	// nada -> click en una ciudad -> relacionando ciudad
+	// nada -> click en un continente -> relacionando continente
+	// relacionando ciudad -> click en un lugar virgen -> crea ciudad y relacion -> nada
+	// relacionando ciudad -> click en una ciudad sin relacion -> relaciona ciudad -> nada
+	// relacionando ciudad -> click en una ciudad con relacion -> anula relacion -> nada
+	// relacionnado ciudad -> click en un continente sin relacion ... y asi
+	// nada -> doble click en una ciudad / continente -> renombrar -> nombre vacio -> eliminar -> nada
+
+	// relacionando continente -> click en un continente -> error -> nada
+	// hay un area para poner el nombre y un boton para salvar (accion que se hara en el servidor)
+
+	// hay una zona rectangular 2d arriba para crear y relacionar continentes
+}
