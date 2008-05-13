b20d06ab1fa07f97118d86611959e5934fe67558
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-05-13 23:49:21 +0000

    Agregue "paquete" source/client/common-ui que contendrà clases y funciones comunes a la interfaz grafica tanto del cliente como del editor de mapas. Agreguè algunas clases para almacenar informaciòn del contexto de la User Interface asì como de la informaciòn del mouse y del teclado.
    TODO -- Falta definir e implementar atributos y mètodos de las clases UIHandler, UIMouseInformation, UIKeyboardInformation.

---
+++ source/client/common-ui/uihandler.h
@@ -0,0 +1,13 @@
+#ifndef __UIHANDLER_H__
+#define __UIHANDLER_H__
+
+/**
+ * Clase cuyo propòsito es identificar a un control grafico dentro de una
+ * interfaz grafica Inmediate Mode GUI.
+ */
+class UIHandler {
+
+};
+
+#endif /** __UIHANDLER_H__ */
+
---
+++ source/client/common-ui/uikeyboardinformation.h
@@ -0,0 +1,14 @@
+#ifndef __UIKEYBOARDINFORMATION_H__
+#define __UIKEYBOARDINFORMATION_H__
+
+/**
+ * Clase cuyo propòsito es almacenar informaciòn del estado del
+ * keyboard, como que teclas fueron presionadas y cuales
+ * liberadas.
+ */
+class UIKeyboardInformation {
+
+};
+
+#endif /** __UIKEYBOARDINFORMATION_H__ */
+
