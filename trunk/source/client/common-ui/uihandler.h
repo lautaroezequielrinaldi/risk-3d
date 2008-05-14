8620a010c353d7520c33b99c7822035f02540544
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-05-14 03:16:58 +0000

    Agreguè implementaciòn de UIHandler que bàsicamente guarda informaciòn del ID owner o ID parent del elemento gràfico actual dentro de la interfaz gràfica, el ID del elemento gràfico actual dentro de la interfaz gràfica y el ìndice de dicho elemento gràfico dentro de un array de elementos gràficos, por ejemplo cuando creamos elementos gràficos dentro de un ciclo for o while.
    Ademàs defino una macro llamada GEN_ID que computa un ID ùnico para un archivo
    fuente y lìnea de còdigo determinada. Si se invoca a la misma macro varias
    veces desde el mismo archivo fuente y misma lìnea de còdigo, devolverà el mismo
    ID, para esos casos se hace uso del atributo arrayIndex del UIHandler ademàs del ID generado por GEN_ID

--- trunk/source/client/common-ui/uihandler.h
+++ trunk/source/client/common-ui/uihandler.h
@@ -2,11 +2,79 @@
 #define __UIHANDLER_H__
 
 /**
+ * Macro GEN_ID que devuelve o genera un ID de elemento grafico ùnico.
+ */
+#define STRINGERMACRO( x ) #x 
+#define GEN_ID (int)(__FILE__ STRINGERMACRO(__LINE__))
+
+/**
  * Clase cuyo propòsito es identificar a un control grafico dentro de una
  * interfaz grafica Inmediate Mode GUI.
  */
 class UIHandler {
+    /**
+     * Atributos privados de la clase UIHandler.
+     */
+    private:
+        /**
+         * Almacena el ID parent o ID owner del elemento gràfico actual.
+         * Almacena 0 si el elemento gràfico actual no tiene elemento gràfico
+         * parent o elemento gràfico owner.
+         */
+        int ownerId;
+        /**
+         * Almacena el ID del elemento gràfico actual.
+         */
+        int elementId;
+        /**
+         * Almacena el ìndice del elemento gràfico en el array de elementos
+         * gràficos. Si el elemento gràfico no se encuentra dentro de un array
+         * de elementos gràficos su ìndice serà 0.
+         */
+        int arrayIndex;
 
+    /**
+     * Mètodos pùblicos de la clase UIHandler.
+     */
+    public:
+        /**
+         * Constructor de la clase UIHandler.
+         * Construye un UIHandler a partir del ID de elemento gràfico actual.
+         */
+        UIHandler(int elementId = GEN_ID);
+        /**
+         * Mètodo cuyo propòsito es obtener el ID parent o ID owner del elemento
+         * gràfico actual.
+         */
+        int getOwnerId();
+        /**
+         * Mètodo cuyo propòsito es establecer el ID parent o ID owner del
+         * elemento gràfico actual.
+         */
+        void setOwnerId(int ownerId);
+        /**
+         * Mètodo cuyo propòsito es obtener el ID del elemento gràfico actual.
+         */
+        int getElementId();
+        /**
+         * Mètodo cuyo propòsito es establecer el ID del elemento gràfico
+         * actual.
+         */
+        void setElementId(int elementId);
+        /**
+         * Mètodo cuyo propòsito es obtener el ìndice del elemento gràfico
+         * actual dentro del array de elementos gràficos.
+         */
+        int getArrayIndex();
+        /**
+         * Mètodo cuyo propòsito es establecer el ìndice  del elemento gràfico
+         * actual dentro del array de elementos gràficos.
+         */
+        void setArrayIndex(int arrayIndex);
+        /**
+         * Destructor virtual de la clase UIHandler.
+         */
+        ~UIHandler();
 };
 
 #endif /** __UIHANDLER_H__ */
