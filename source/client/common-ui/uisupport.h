a1135a3f542396b1203bd4035372adce688d25e5
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-05-14 01:40:19 +0000

    Agregue implementaciòn de la clase UISupport con el mètodo isMouseOver para verificar si el mouse està posicionado sobre una region rectangular determinada.

---
+++ source/client/common-ui/uisupport.h
@@ -0,0 +1,24 @@
+#ifndef __UISUPPORT_H__
+#define __UISUPPORT_H__
+
+/**
+ * Clase cuyo propòsito es brindar soporte y facilidades a còdigo de interfaz
+ * grafica de usuario. Proveyendo metodos para saber si el mouse se encuentra
+ * posicionado sobre un àrea determinada de la pantalla y demàs funcionalidad
+ * relacionada. Es una clase estàtica.
+ */
+class UISupport {
+    /**
+     * Mètodos pùblicos de la clase UISupport.
+     */
+    public:
+        /**
+         * Mètodo para verificar si el mouse se encuentra sobre una posiciòn
+         * determinada de la pantalla, delimitada por x, y, width, height.
+         */
+        bool isMouseOver(const UIMouseInformation& mouseInformation, int x,
+            int y, int width, int height);
+};
+
+#endif /** __UISUPPORT_H__ */
+
