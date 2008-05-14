0076ada908e07e4abdc6811c99fcfe3e5e9de358
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-05-14 03:16:58 +0000

    Agreguè implementaciòn de UIHandler que bàsicamente guarda informaciòn del ID owner o ID parent del elemento gràfico actual dentro de la interfaz gràfica, el ID del elemento gràfico actual dentro de la interfaz gràfica y el ìndice de dicho elemento gràfico dentro de un array de elementos gràficos, por ejemplo cuando creamos elementos gràficos dentro de un ciclo for o while.
    Ademàs defino una macro llamada GEN_ID que computa un ID ùnico para un archivo
    fuente y lìnea de còdigo determinada. Si se invoca a la misma macro varias
    veces desde el mismo archivo fuente y misma lìnea de còdigo, devolverà el mismo
    ID, para esos casos se hace uso del atributo arrayIndex del UIHandler ademàs del ID generado por GEN_ID

---
+++ source/client/common-ui/uihandler.cpp
@@ -0,0 +1,37 @@
+#include "uihandler.h"
+
+UIHandler::UIHandler(int elementId = GEN_ID):
+    ownerId(0),
+    elementId(elementId),
+    arrayIndex(0) {
+    // No realiza ninguna acciòn.
+}
+
+int UIHandler::getOwnerId() {
+    return this->ownerId;
+}
+
+void UIHandler::setOwnerId(int ownerId) {
+    this->ownerId = ownerId;
+}
+
+int UIHandler::getElementId() {
+    return this->elementId;
+}
+
+void UIHandler::setElementId(int elementId) {
+    this->elementId = elementId;
+}
+
+int UIHandler::getArrayIndex() {
+    return this->arrayIdex;
+}
+
+void UIHandler::setArrayIndex(int arrayIndex) {
+    this->arrayIndex = arrayIndex;
+}
+
+UIHandler::~UIHandler() {
+    // No realiza ninguna acciòn.
+}
+
