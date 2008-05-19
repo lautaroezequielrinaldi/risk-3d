24bb208872bfa209c75df8e30a7c6ca333429d6f
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-05-19 04:18:17 +0000

    Agregue la clase Continente y su implementaciòn.

---
+++ source/common/model/continente.cpp
@@ -0,0 +1,43 @@
+#include "continente.h"
+
+Continente::Continente(const std::string& nombre):
+    nombre(nombre),
+    paises() {
+    // No realiza ninguna acciòn.
+}
+
+std::string Continente::getNombre() {
+    return this->nombre;
+}
+
+void Continente::setNombre(const std::string& nombre) {
+    this->nombre = nombre;
+}
+
+void Continente::agregarPais(const ReferenceCountPtr<Pais>& pais) {
+    this->paises.push_back(pais);
+}
+
+void Continente::removerPais(const ReferenceCountPtr<Pais>& pais) {
+    this->paises.remove(pais);
+}
+
+ReferenceCountPtr<Pais> Continente::obtenerPais(const std::string& nombre) {
+    // Itero por todos los paises.
+    for (int counter = 0; counter < this.paises.size(); ++counter) {
+        // Obtengo pais actual.
+        ReferenceCountPtr<Pais> actual = this.paises[counter];
+        // Si los nombres coinciden.
+        if (*actual.getNombre() == nombre) {
+            // Devuelvo pais actual.
+            return actual;
+        }
+    }
+    // Si no encuentro al pais, devuelvo NULL.
+    return NULL;
+}
+
+Continente::~Continente() {
+    // No realiza ninguna acciòn.
+}
+
