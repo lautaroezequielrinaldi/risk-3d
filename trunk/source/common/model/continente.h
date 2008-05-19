090d47a220ac235fd668e50a66bbfd26c58f2288
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-05-19 04:18:17 +0000

    Agregue la clase Continente y su implementaciòn.

---
+++ trunk/source/common/model/continente.h
@@ -0,0 +1,77 @@
+#ifndef __CONTINENTE_H__
+#define __CONTINENTE_H__
+
+#include<string> // Para definiciòn de std::string.
+#include<vector> // Para definiciòn de std::vector.
+#include "pais.h" // Para definiciòn de Pais.
+#include "../smartpointer/referencecountptr.h" // Para definiciòn de
+// ReferenceCountPtr.
+
+/**
+ * Clase cuyo propòsito es representar un continente dentro de nuestro
+ * modelo.
+ */
+class Continente {
+    /**
+     * Atributos privados de la clase Continente.
+     */
+    private:
+        /**
+         * Almacena el nombre del Continente.
+         */
+        std::string nombre;
+        /**
+         * Almacena la lista de paises del Continente.
+         */
+        std::vector< ReferenceCountPtr<Pais> > paises;
+
+    /**
+     * Mètodos privados de la clase Continente.
+     */
+    private:
+        /**
+         * Constructor copia de la clase Continente.
+         */
+        Continente(const Continente& otherInstance);
+        /**
+         * Operador de asignaciòn de la clase Continente.
+         */
+        Continente& operator=(const Continente& otherInstance);
+
+    /**
+     * Mètodos pùblicos de la clase Continente.
+     */
+    public:
+        /**
+         * Constructor de la clase Continente.
+         */
+        Continente(const std::string& nombre);
+        /**
+         * Mètodo cuyo propòsito es obtener el nombre del Continente.
+         */
+        std::string getNombre();
+        /**
+         * Mètodo cuyo propósito es establecer el nombre del Continente.
+         */
+        void setNombre();
+        /**
+         * Mètodo cuyo propòsito es agregar un pais al Continente.
+         */
+        void agregarPais(const ReferenceCountPtr<Pais>& pais);
+        /**
+         * Mètodo cuyo propòsito es remover un pais del Continente.
+         */
+        void removerPais(const ReferenceCountPtr<Pais>& pais);
+        /**
+         * Mètodo cuyo propòsito es obtener un pais del Continente.
+         * NULL si el pais no se encuentra.
+         */
+        ReferenceCountPtr<Pais> obtenerPais(const std::string& nombre);
+        /**
+         * Destructor virtual de la clase Continente.
+         */
+        ~Continente();
+};
+
+#endif /** __CONTINENTE_H__ */
+
