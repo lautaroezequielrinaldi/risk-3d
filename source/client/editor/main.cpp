60c1268b3c84a1771a25820c05ed8c22cbf2d099
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-05-23 03:22:22 +0000

    Modifique el Main de Editor

--- source/client/editor/main.cpp
+++ source/client/editor/main.cpp
@@ -1,11 +1,4 @@
-#include "application.h"
-
 int main (int argc, char ** argv){
-    Application app;
-
-    app.execute();
-
-    return 0;
 	// estados
 	// nada -> click en un lugar virgen -> crea ciudad
 	// nada -> click en una ciudad -> relacionando ciudad
