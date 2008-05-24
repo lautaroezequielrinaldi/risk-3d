6cae49f398a98aea5dedcf376beb7eccc833a916
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-05-24 04:00:32 +0000

    Modifique clases comentando logica propuesta asi compila, y cambie algunos includes que no compilaban.

--- trunk/source/protocols/commands/commandfactoryfactory.cpp
+++ trunk/source/protocols/commands/commandfactoryfactory.cpp
@@ -10,24 +10,24 @@
 
 
 CommandFactoryFactory::CommandFactoryFactory() {
-	// crea una instancia de cada objeto y
+/*	// crea una instancia de cada objeto y
 	//  lo guarda en un map con el nombre de la clase
 	//  como clave
 	ReferenceCountPtr<CommandFactory> attackfactory();
 	objectmap->put("Attack", attackfactory);
 
-
+*/
 }	
 /*
  * Esto es muy borrador, tambien se puede usar un switch
  *
  */
 
-ReferenceCountPtr<CommandFactory> CommandFactoryFactory::build(const std::String& type, const std::String& xml) {
-	dom=libxml(xml);
+ReferenceCountPtr<CommandFactory> CommandFactoryFactory::build(const std::string& type, const std::string& xml) {
+/*	dom=libxml(xml);
 	commandfactory = objectmap->get(type); 
 	commandfactory->getCommand(type,dom);
 	return commandfactory;
+*/
 }
-
 
