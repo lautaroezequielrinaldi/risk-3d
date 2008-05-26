c048e0ca710696e93fac38cfe739e1bd9ec5d62b
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-05-26 01:43:11 +0000

    Agregue a editor una referencia a travez de smart pointer al mapa, luego cada frame agrega continentes y muestra continentes al y desde el mapa que contiene el editor.

--- trunk/source/client/editor/editor.cpp
+++ trunk/source/client/editor/editor.cpp
@@ -1,7 +1,17 @@
 #include "editor.h"
 
-Editor::Editor() {
-    // No realiza ninguna acciòn.
+Editor::Editor():
+    mapa() {
+    // Inicializa el mapa que va a usar el editor.
+    this->mapa = new Mapa();
+}
+
+void Editor::setMapa(const ReferenceCountPtr<Mapa>& mapa) {
+    this->mapa = mapa;
+}
+
+ReferenceCountPtr<Mapa> Editor::getMapa() {
+    return this->mapa;
 }
 
 Editor::~Editor() {
