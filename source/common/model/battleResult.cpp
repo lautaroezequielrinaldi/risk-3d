7e62e6d97adaa2fecb56566edf58e5202a7cf6cf
anihojman <anihojman@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-05-27 08:23:40 +0000

    Agregue clases Battle y BattleResult que basicamente se encargaran de manejar la batalla. No estan implementadas aun.

---
+++ source/common/model/battleResult.cpp
@@ -0,0 +1,18 @@
+#include "battleResult.h"
+
+BattleResult::BattleResult()
+{
+}
+
+BattleResult::~BattleResult()
+{
+}
+
+std::string BattleResult::serialize(){
+	
+}
+
+void* BattleResult::hydrate(ReferenceCountPtr<Mapa>& mapa,std::string fileName){
+	
+}
+
