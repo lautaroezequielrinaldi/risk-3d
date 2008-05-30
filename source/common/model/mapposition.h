cd4688ac1b9afc54e3acd742a814f436ed0bb286
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-05-30 00:52:54 +0000

    Agregue la clase MapPosition para representar una posicion X,Y dentro del mapa.

---
+++ source/common/model/mapposition.h
@@ -0,0 +1,49 @@
+#ifndef __MAPPOSITION_H__
+#define __MAPPOSITION_H__
+
+class MapPosition {
+    /**
+     * Atributos privados de la clase MapPosition.
+     */
+    private:
+        /**
+         * Almacena la posicion x dentro del mapa.
+         */
+        int x;
+        /**
+         * Almacena la posicion y dentro del mapa.
+         */
+        int y;
+
+    /**
+     * Mètodos pùblicos de la clase MapPosition.
+     */
+    public:
+        /**
+         * Constructor de la clase MapPosition.
+         */
+        MapPosition(const int& x, const int& y);
+        /**
+         * Establece la posicion x dentro del mapa.
+         */
+        void setX(const int& x);
+        /**
+         * Obtiene la posicion x dentro del mapa.
+         */
+        int& getX();
+        /**
+         * Establece la posicion y dentro del mapa.
+         */
+        void setY(const int& y);
+        /**
+         * Obtiene la posicion y dentro del mapa.
+         */
+        int& getY();
+        /**
+         * Destructor virtual de la clase MapPosition.
+         */
+        virtual ~MapPosition();
+};
+
+#endif /** __MAPPOSITION_H__ */
+
