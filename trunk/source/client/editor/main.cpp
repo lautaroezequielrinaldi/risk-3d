41955be16d26a3383d392af9582dededef84100f
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-06-01 02:30:50 +0000

    Agregue algunas cosas de prueba para conectar varios paises y que me los muestre en pantalla con lineas y circulos.

--- trunk/source/client/editor/main.cpp
+++ trunk/source/client/editor/main.cpp
@@ -16,18 +16,26 @@ int main(int argc, char** argv) {
     ReferenceCountPtr<Continente> continente = new Continente("ASIA", 15);
     MapPosition position(50,50);
     MapPosition position2(100,100);
+    MapPosition position3(400, 200);
 
     ReferenceCountPtr<Pais> china = new Pais("CHINA", position);
     continente->agregarPais(china);
     ReferenceCountPtr<Pais> birmania = new Pais("BIRMANIA", position2);
+    ReferenceCountPtr<Pais> japon = new Pais("JAPON", position3);
+
     china->agregarAdyacente(birmania);
     birmania->agregarAdyacente(china);
+    china->agregarAdyacente(japon);
+    japon->agregarAdyacente(china);
+    japon->agregarAdyacente(birmania);
+    birmania->agregarAdyacente(japon);
 
     ReferenceCountPtr<Editor> editor( new Editor());
 
     editor->getMapa()->agregarContinente(continente);
     editor->getMapa()->agregarPais(china);
     editor->getMapa()->agregarPais(birmania);
+    editor->getMapa()->agregarPais(japon);
 
     AddContinentFrame addFrame(editor);
     ViewContinentsFrame viewFrame(editor);
