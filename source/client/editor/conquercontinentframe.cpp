289dcab4e684a1038258ecbc9533c6451eacc092
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-06-12 14:15:08 +0000

    Agregue una clase comun para todos los frames de cartas de juego, asi reutilizan controles y codigo.

--- source/client/editor/conquercontinentframe.cpp
+++ source/client/editor/conquercontinentframe.cpp
@@ -3,18 +3,13 @@
 ConquerContinentFrame::ConquerContinentFrame(
 	const ReferenceCountPtr<Editor>& editor):
 	Observer(),
-	Gtk::Frame(),
-	editor(editor),
-	verticalBox(),
-	gameCardNameLabel(),
-	gameCardNameEntry(),
+	GameCardFrame(editor),
 	continentLabel(),
 	continentTreeModel(),
 	continentComboBox(),
 	addGameCardButton() {
 	// Establece el titulo del frame.
 	this->set_label("Carta para comquistar continente");
-
 	// Se agrega como observador del editor.
 	if (this->editor != NULL) {
 		this->editor->registerObserver(this);
@@ -25,37 +20,15 @@ ConquerContinentFrame::ConquerContinentFrame(
 	// Inicializa el combo box para la lista de continentes.
 	this->initializeContinentComboBox();
 
-	// Establece el texto del label para nombre de carta de juego.
-	this->gameCardNameLabel.set_text("Nombre de carta de juego:");
 	// Establece el texto del label para continente.
 	this->continentLabel.set_text("Seleccione continente:");
-	// Establece el texto del boton para agregar carta de juego.
-	this->addGameCardButton.set_label("Agregar Carta de Juego");
 
-	// Agrega el label para nombre de carta de juego al vertical box.
-	this->verticalBox.pack_start(this->gameCardNameLabel,
-		Gtk::PACK_SHRINK);
-	// Agrega el entry para nombre de carta de juego al vertical box.
-	this->verticalBox.pack_start(this->gameCardNameEntry,
-		Gtk::PACK_SHRINK);
 	// Agrega el label para el continente al vertical box.
 	this->verticalBox.pack_start(this->continentLabel,
 		Gtk::PACK_SHRINK);
 	// Agrega el combo box para la lista de continentes al vertical box.
 	this->verticalBox.pack_start(this->continentComboBox,
 		Gtk::PACK_SHRINK);
-	// Agrega el boton para agregar carta de juego al vertical box.
-	this->verticalBox.pack_start(this->addGameCardButton,
-		Gtk::PACK_SHRINK);
-
-	// Agrega el vertical box al frame.
-	this->add(this->verticalBox);
-
-	// Asocia senial signal_clicked del boton addGameCardButton al menejador de
-	// la senial.
-	this->addGameCardButton.signal_clicked().connect(sigc::mem_fun(*this,
-		&ConquerContinentFrame::onAddGameCardButtonClicked));
-
 	// Popula la lista de continentes.
 	this->populate();
 }
@@ -74,20 +47,20 @@ void ConquerContinentFrame::populate() {
     this->continentTreeModel->clear();
 
 	// Si el editor no es null y el mapa no es null.
-    if (editor != NULL && editor->getMapa() != NULL) {
+    if (this->editor != NULL && this->editor->getMapa() != NULL) {
 		// Obtengo el map del modelo.
-        ReferenceCountPtr<Mapa> map = editor->getMapa();
-        // Defino iterador de continentes.
+        ReferenceCountPtr<Mapa> map = this->editor->getMapa();
+		// Defino iterador de continentes.
         Mapa::IteradorContinente continentIter;
-        // Itero por cada continente.
+		// Itero por cada continente.
         for (continentIter = map->primerContinente();
 		    continentIter != map->ultimoContinente(); ++continentIter) {
-           // Obtengo continente actual.
-           ReferenceCountPtr<Continente> continent = *continentIter;
-           // Creo fila de lista de continentes.
-           Gtk::TreeModel::Row row = *(this->continentTreeModel->append());
-           // Agrego nombre de continente a fila de lista de continentes.
-           row[continentColumns.continentName] = continent->getNombre();
+			// Obtengo continente actual.
+            ReferenceCountPtr<Continente> continent = *continentIter;
+            // Creo fila de lista de continentes.
+            Gtk::TreeModel::Row row = *(this->continentTreeModel->append());
+            // Agrego nombre de continente a fila de lista de continentes.
+            row[continentColumns.continentName] = continent->getNombre();
 		}
     }
 }
@@ -127,7 +100,7 @@ void ConquerContinentFrame::onAddGameCardButtonClicked() {
 }
                 
 void ConquerContinentFrame::setEditor(const ReferenceCountPtr<Editor>& editor) {
-	this->editor = editor;
+	GameCardFrame::setEditor(editor);	
 
     // Se agrega como observador del editor.
     if (this->editor != NULL) {
@@ -139,7 +112,7 @@ void ConquerContinentFrame::setEditor(const ReferenceCountPtr<Editor>& editor) {
 }
 
 ReferenceCountPtr<Editor>& ConquerContinentFrame::getEditor() {
-	return this->editor;
+	return GameCardFrame::getEditor();
 }
 
 void ConquerContinentFrame::update(Subject* subject) {
