a8ff953590bd51b92de2376a8c9b98ea768bb75e
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-06-14 22:23:23 +0000

    Agregue implementacionde hexcoder

--- source/common/parser/hexcoder.h
+++ source/common/parser/hexcoder.h
@@ -1,3 +1,25 @@
 #ifndef __HEXCODER__
 #define __HEXCODER__
+
+#include<string> // Para definicion de std::string.
+
+/**
+ * Clase para encodear y desencodear en base64
+ */
+class HexCoder {
+	/**
+	 * Metodos publicos de la clase HexCoder.
+	 */
+	public:
+		/**
+		 * Encodea una cadena de bytes a base64 encoding.
+		 */
+		static std::string encode(unsigned char const* , unsigned int len);
+		/**
+		 * Desencodea una cadena de bytes de base64 encoding.
+		 */
+		static std::string decode(std::string const& s);
+};
+
 #endif
+
