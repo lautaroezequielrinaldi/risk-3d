eaeb1168e9d143b9d2f0ebc116d628cf20f4cc08
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-06-14 20:41:01 +0000

    Agregue ventana principal y menu principal

--- source/client/editor/editor.cpp
+++ source/client/editor/editor.cpp
@@ -1,7 +1,8 @@
 #include "editor.h"
 
-Editor::Editor():
-    mapa() {
+Editor::Editor(const std::string& imageFileName):
+    mapa(),
+	imageFileName(imageFileName) {
     // Inicializa el mapa que va a usar el editor.
     this->mapa = new Mapa();
 }
@@ -12,6 +13,14 @@ void Editor::setMapa(const ReferenceCountPtr<Mapa>& mapa) {
 
 ReferenceCountPtr<Mapa>& Editor::getMapa() {
     return this->mapa;
+}
+
+void Editor::setImageFileName(const std::string& imageFileName) {
+	this->imageFileName = imageFileName;
+}
+
+std::string& Editor::getImageFileName() {
+	return this->imageFileName;
 }
 
 Editor::~Editor() {
