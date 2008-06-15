7d6b64b5b450df66e5adf9a36e93000cf01048d8
anihojman <anihojman@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-06-15 16:54:09 +0000

    arregle errores de compilacion MOVI TODO LO REFERIDO A STATE QUE ESTABA EN SERVER, A LA CARPETA STATE , unifique los state.h y state.cpp ya que habia 2 de cada uno. UNIFIQUE TODOS LOS COMANDOS EN common/commands

--- trunk/source/common/state/state.h
+++ trunk/source/common/state/state.h
@@ -1,6 +1,8 @@
 #ifndef __STATE_H__
 #define __STATE_H__
 
+#include <string>
+
 class Attack;
 class Defend;
 class JoinGame;
@@ -10,19 +12,39 @@ class Populate;
 class ReadyToPlay;
 
 class State {
-	public:
-	State();
-	virtual ~State();
 
-     virtual bool attack(Attack & command) = 0;
-     virtual bool defend(Defend & command) = 0;
-     virtual bool joinGame(JoinGame & command) = 0;
-     //bool kill(Kill & move);
-     virtual bool move(Move & command) = 0;
-     virtual bool selectMap(SelectMap & command) = 0;
-     //bool setOwner(SetOwner & command);
-     virtual bool populate(Populate & command) = 0;
-     virtual bool readyToPlay(ReadyToPlay & command) = 0;
+	private:
+	
+		std::string name;
+
+
+	public:
+		
+		/**
+		 * Constructor de la clase
+		 */
+		State();
+		
+		/**
+		 * Destructor de la clase
+		 */
+		virtual ~State();
+
+
+		virtual State * getNext()=0;
+		std::string getName();
+
+
+	    virtual bool attack(Attack & command) = 0;
+	    
+	    virtual bool defend(Defend & command) = 0;
+	    virtual bool joinGame(JoinGame & command) = 0;
+	    //bool kill(Kill & move);
+	    virtual bool move(Move & command) = 0;
+	    virtual bool selectMap(SelectMap & command) = 0;
+	    //bool setOwner(SetOwner & command);
+	    virtual bool populate(Populate & command) = 0;
+	    virtual bool readyToPlay(ReadyToPlay & command) = 0;
 };
 
 #endif /** __STATE_H__ */
