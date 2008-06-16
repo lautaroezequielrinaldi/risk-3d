ff98976b97d8995d260dd5990fa4c7d04c8c1f1a
carlos.pantelides <carlos.pantelides@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-06-16 19:16:44 +0000

    nuevo estado waiting first player

---
+++ source/common/state/waitingfirstplayer.cpp
@@ -0,0 +1,19 @@
+#include "waitingfirstplayer.h"
+
+WaitingFirstPlayer::WaitingFirstPlayer()
+{
+}
+
+WaitingFirstPlayer::~WaitingFirstPlayer()
+{
+}
+
+
+
+bool WaitingFirstPlayer::joinGame(JoinGame & command){
+	
+	return false;
+}
+
+
+
