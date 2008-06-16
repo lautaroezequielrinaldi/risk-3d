7e378c2b79dc5fe77795c8e076509c2a21e7e5f6
carlos.pantelides <carlos.pantelides@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-06-16 19:16:44 +0000

    nuevo estado waiting first player

---
+++ trunk/source/common/state/waitingfirstplayer.h
@@ -0,0 +1,18 @@
+#ifndef WAITINGFIRSTPLAYER_H_
+#define WAITINGFIRSTPLAYER_H_
+
+#include "state.h"
+
+class WaitingFirstPlayer : public State
+{
+
+	public:
+
+		WaitingFirstPlayer();
+		
+		~WaitingFirstPlayer();
+		
+	                   virtual bool joinGame(JoinGame & command); 
+};
+
+#endif /*WAITINGFIRSTPLAYER_H_*/
