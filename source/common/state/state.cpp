cf40d3392b48368c6bbeb7c62614fdc53100c685
carlos.pantelides <carlos.pantelides@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-06-22 17:25:05 +0000

    mensajes de error

--- source/common/state/state.cpp
+++ source/common/state/state.cpp
@@ -31,45 +31,69 @@ std::string State::getName(){
 
 
 bool State::attack(Attack & command){
+	command.setMainMsg("Intento de ataque en momento inoportuno");
+	command.setSecMsg("El jugador ... intentó atacar cuando no le tocaba");
 	return false;
 }
 
 bool State::defend(Defend & command){
+	command.setMainMsg("Intento de defensa en momento inoportuno");
+	command.setSecMsg("El jugador ... intentó defenderse cuando no le tocaba");
+
 	return false;
 }
 
 //virtual void State::occupy(Occupy & command);
 
 bool State::joinGame(JoinGame & command){
+	command.setMainMsg("...");
+	command.setSecMsg("Un jugador intentó conectarse con el partido iniciado");
 	return false;
 }
 //virtual bool State::kill(Kill & move);
 bool State::move(Move & command){
+	command.setMainMsg("Intento de movimiento en momento inoportuno");
+	command.setSecMsg("El jugador ... intentó moverse cuando no le tocaba");
+
 	return false;
 }
 bool State::selectMap(SelectMap & command){
+	command.setMainMsg("Intento de selección de mapa en momento inoportuno");
+	command.setSecMsg("El jugador ... intentó seleccionar mapa cuando no le tocaba");
+
         return false;
 }
 
 //bool virtual setOwner(SetOwner & command);
 bool State::populate(Populate & command){
+	command.setMainMsg("Intento de poblar en momento inoportuno");
+	command.setSecMsg("El jugador ... intentó poblar cuando no le tocaba");
+
      return false;
 }
 
 bool State::readyToPlay(ReadyToPlay & command){
+	command.setMainMsg("Intento de aceptar el juego en momento inoportuno");
+	command.setSecMsg("El jugador ... intentó aceptar el juego cuando no le tocaba");
+
 	return false;
 }
 
 bool State::noMore(NoMore & command){
+	command.setMainMsg("Intento de terminar etapa en momento inoportuno");
+	command.setSecMsg("El jugador ... intentó terminar etapa cuando no le tocaba");
+
 	return false;
 }
 
 bool State::surrender(Surrender & command){
-	return false;
+	// debe deshabilitar al jugador y convertir sus paises en republicas independientes
+	return true;
 }
 
 bool State::quit(Quit & command){
-        return false;
+	// debe aplicar surrender y ademas cortar la conexión
+        return true;
 }
 
 
