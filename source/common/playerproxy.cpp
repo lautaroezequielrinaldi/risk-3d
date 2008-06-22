6acafb9912a1dca3290dbbf2047f2cdf514135cb
carlos.pantelides <carlos.pantelides@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-06-22 23:42:21 +0000

    analisis estados y esbozos de completar algunos estados

--- source/common/playerproxy.cpp
+++ source/common/playerproxy.cpp
@@ -1,45 +1,33 @@
 
 #include "playerproxy.h"
-
+#include<sstream>
 #include <iostream>
 void * PlayerProxy::run() {
-	std::string msg("bienvenido\n");
+	std::string msg1("bienvenido\n");
+	socket->write(msg1);
 
-	socket->write(msg);
+	std::stringstream msg;
+	unsigned int msgLen;
+	std::string commandName;
+	std::string commandXml;
 
 	while (true) {
+		std::cerr << "PlayerProxy leyendo encabezado " << std::endl;
+		// deshardcodear este 32
+		msg << socket->full_read(32);
+		msg >> msgLen;
+		msg >> commandName;
 
-		// deshardcodear este 30
-		msg=socket->full_read(30);
-		std::cerr << msg << std::endl;
-		unsigned int msgLen = 0; //leer cuanto mide el resto del mensaje
-		std::string commandName = "JoinGame"; //leer que comando es
-		std::string commandXml = "<name>Mauro</name>";//socket->full_read(msgLen);
+		std::cerr << "longitud " << msgLen << " nombre " << commandName << std::endl;
+		commandXml = socket->full_read(msgLen);
 		
+		std::cerr << "serializacion " << commandXml << std::endl;
 		gameManager->execute(commandName, commandXml);
 
-		// crear el commmand
-		// command->execute()
-
-/*		for (players_iterator =players.begin();
-				players_iterator != players.end();
-				++players_iterator) {
-			PlayerProxy * playerProxy =  *players_iterator;
-			playerProxy->notify(msg);
-		}
-*/
 	}
-		
-
-	//   cuando llega el mensaje,
-	//   se obtiene un lock sobre el estado.
-	//   dependiendo del estado, al cual todos los jugadores acceden
-	//   se toma alguna accion, que puede involucrar escribir en todos los demas jugadores
-	//   devuelve el lock
-	
-	//delete[] data;
 	return 0;
 }
+
 void PlayerProxy::notify(const std::string & msg) {
 	socket->write(msg);
 }
--- source/common/state/waitingmapselection.cpp
+++ source/common/state/waitingmapselection.cpp
@@ -9,17 +9,25 @@ WaitingMapSelection::~WaitingMapSelection()
 }
 
 bool WaitingMapSelection::joinGame(JoinGame & command) {
+	// si hay lugar
+	//    aceptarlo
+	//    YouAre(n) 
 	return false;
 }
 
 
 bool WaitingMapSelection::readyToPlay(ReadyToPlay & command) {
+	// marcar como ready to play
 	return false;
 }
 
 
 bool WaitingMapSelection::selectMap(SelectMap & command){
-	
+	// ver si es el jugador actual
+	// si ahora no hay lugar o todos estan ready to play
+	//    Map
+	//     pasar a simplePopulating
+	//     seleccionar primer jugador y TurnToOcuppy	
 	return false;
 }
 
