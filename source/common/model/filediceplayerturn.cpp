4ce3b62765fcae36a0f5fcde42b01b370279d9d5
anihojman <anihojman@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-06-23 23:51:44 +0000

    implemente en turnManager la part que guarda el ultimo jugador de la ronda cambio nombre clase en filediceplayerturn

--- source/common/model/filediceplayerturn.cpp
+++ source/common/model/filediceplayerturn.cpp
@@ -1,28 +1,33 @@
 #include "filediceplayerturn.h"
+#include <iostream>
+using namespace std;
 
-fileDicePlayerTurn::fileDicePlayerTurn(std::string fileName)
+FileDicePlayerTurn::FileDicePlayerTurn(std::string fileName):Dice()
 {
-	
+			
+	std::string pathName = "/home/analia/Escritorio/risk3d/source/common/model/"+fileName;
 	//abro archivo del cual se levantaran los valores que toma el dado
-	this->file.open(fileName.c_str());
+	this->file.open(pathName.c_str());
+	
+	
 }
-
-fileDicePlayerTurn::~fileDicePlayerTurn()
+FileDicePlayerTurn::~FileDicePlayerTurn()
 {
 	// si el archivo esta abierto, lo cierro- verificacion automatica.
 	this->file.close();
 }
 
-int fileDicePlayerTurn::roll(){
+int FileDicePlayerTurn::roll(){
+	
 	
 	std::string linea;
 	int valor;
 	char * strValor=NULL;
-	
+
 	
 	// obtengo valor del archivo
 	getline(this->file, linea);
-	
+
 	strValor = new char (linea[0]);
 	valor = atoi (strValor);
 	
@@ -30,7 +35,7 @@ int fileDicePlayerTurn::roll(){
 	delete strValor;
 
 	return valor;
-	
+
 }
 
 
