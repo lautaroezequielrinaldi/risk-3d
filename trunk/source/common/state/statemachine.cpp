245f8ae39b1e7f3a744184441faa2ec0b46f0715
carlos.pantelides <carlos.pantelides@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-06-25 13:07:00 +0000

    refactorizacion StateMachine

--- trunk/source/common/state/statemachine.cpp
+++ trunk/source/common/state/statemachine.cpp
@@ -1,18 +1,9 @@
 #include "statemachine.h"
 // #include "todos los estados"
 
-StateMachine::StateMachine(ReferenceCountPtr<GameManager> & gameManager){
-	states.insert(std::make_pair("attacking",new Attacking(gameManager,"attacking")));
-        states.insert(std::make_pair("defending",new Defending(gameManager,"defending")));
-        states.insert(std::make_pair("moving",new Moving(gameManager,"moving")));
-        states.insert(std::make_pair("occupying",new Occupying(gameManager,"occupying")));
-        states.insert(std::make_pair("simplePopulating",new SimplePopulating(gameManager,"simplePopulating")));
-        states.insert(std::make_pair("populating",new Populating(gameManager,"populating")));
-        states.insert(std::make_pair("waitingPlayer",new WaitingPlayer(gameManager,"waitingPlayer")));
-        states.insert(std::make_pair("waitingFirstPlayer",new WaitingFirstPlayer(gameManager,"waitingFirstPlayer")));
-        states.insert(std::make_pair("waitingMapSelection",new WaitingMapSelection(gameManager,"waitingMapSelection")));
-	states.insert(std::make_pair("waiting",new Waiting(gameManager,"waiting")));
 
+// esto se va a la clase derivada....
+StateMachine::StateMachine(ReferenceCountPtr<GameManager> & gameManager){
 
 }
 
