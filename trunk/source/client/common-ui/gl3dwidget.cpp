81e31d45589cdc7107a34ee92a7bbde48c1e33f0
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-06-27 23:26:27 +0000

    Modifique preDraw, draw y postDraw de los widgets 3d.

--- trunk/source/client/common-ui/gl3dwidget.cpp
+++ trunk/source/client/common-ui/gl3dwidget.cpp
@@ -1,11 +1,33 @@
 #include "gl3dwidget.h"
 
 void GL3DWidget::preDrawWidget() {
-	// No realiza ninguna accion por el momento.
+    // Obtengo la dimension del viewport.
+	GLfloat viewportDimension[4];
+    glGetFloatv(GL_VIEWPORT, viewportDimension);
+
+	// Guarda en el stack la matriz actual.
+    glPushMatrix();
+    // Guarda en el stack los atributos actuales.
+    glPushAttrib(GL_ALL_ATTRIB_BITS);
+    // Habilita DEPTH BUFFER.
+    glEnable(GL_DEPTH_TEST);
+    // Carga la matriz de proyeccion.
+    glMatrixMode(GL_PROJECTION);
+    // Carga la identidad.
+    glLoadIdentity();
+    // Carga la matriz model view.
+    glMatrixMode(GL_MODELVIEW);
 }
 
 void GL3DWidget::postDrawWidget() {
-	// No realiza ninguna accion por el momento.
+    // Carga del stack los atributos.
+    glPopAttrib();
+    // Carga del stack la matriz de proyeccion.
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+	// Carga del stack la matriz de model view.
+    glMatrixMode(GL_MODELVIEW);
+    glPopMatrix();
 }
 
 GL3DWidget::GL3DWidget(): GLWidget() {
--- trunk/source/client/common-ui/glspherewidget.cpp
+++ trunk/source/client/common-ui/glspherewidget.cpp
@@ -1,13 +1,45 @@
 #include "glspherewidget.h"
 
 void GLSphereWidget::drawWidget(){
-    // Cargo la matriz model view
-	glMatrixMode(GL_MODELVIEW);
+    // Cargo la matriz de proyeccion.
+	glMatrixMode(GL_PROJECTION);
 	// Cargo la identidad
 	glLoadIdentity();
+	// Guardo la matriz en el stack.
+	glPushMatrix();
 	// Translado coordenadas del origen
-	glTranslatef(0.0f, 0.0f, 0.0f);
+	glTranslatef(-0.0f, 0.0f, 0.0f);
+	glRotatef(0.5f, 0.5f, 0.5f, 0.5f);
+	// Cargo la matriz model view.
+	glMatrixMode(GL_MODELVIEW);
+	// Cargo la identidad.
+	glLoadIdentity();
+	// Guardo en el stack la matriz.
+	glPushMatrix();
+	// Establece el color de dibujado.
+	switch (this->getState()) {
+		case GLWIDGET_DISABLED: {
+			Color backgroundColor = this->getBackgroundColor(GLWIDGET_DISABLED);
+			glColor3f(backgroundColor.getRed(), backgroundColor.getGreen(), backgroundColor.getBlue());
+			break;
+		}
+		case GLWIDGET_ACTIVE: {
+            Color backgroundColor = this->getBackgroundColor(GLWIDGET_ACTIVE);
+            glColor3f(backgroundColor.getRed(), backgroundColor.getGreen(), backgroundColor.getBlue());
+            break;
+		}
+		case GLWIDGET_INACTIVE: {
+            Color backgroundColor = this->getBackgroundColor(GLWIDGET_INACTIVE);
+            glColor3f(backgroundColor.getRed(), backgroundColor.getGreen(), backgroundColor.getBlue());
+            break;
+        }
+	}
+	// Dibujo esfera
     gluSphere(sphere, radius, 20, 20);
+	// Cargo matriz.
+	glPopMatrix();
+	// Cargo matriz.
+	glPopMatrix();
 }
 
 GLSphereWidget::GLSphereWidget(const float& radius): GL3DWidget(), sphere(NULL), radius(radius) {
@@ -15,6 +47,8 @@ GLSphereWidget::GLSphereWidget(const float& radius): GL3DWidget(), sphere(NULL),
     gluQuadricDrawStyle (sphere, GLU_FILL);
     gluQuadricNormals(sphere, GLU_SMOOTH);
     gluQuadricTexture(sphere, GL_TRUE);
+	this->setBackgroundColor(GLWIDGET_ACTIVE, Color::RED);
+	this->setBackgroundColor(GLWIDGET_INACTIVE, Color::BLUE);
 }
 
 GLSphereWidget::~GLSphereWidget() {
