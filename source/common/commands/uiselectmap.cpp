5a9b4739406e2871b27f4c1a7f127adf0981e9d4
carlos.pantelides <carlos.pantelides@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-07-01 14:28:01 +0000

    eliminacion de uso de constructores(vector) para uiCommands

--- source/common/commands/uiselectmap.cpp
+++ source/common/commands/uiselectmap.cpp
@@ -6,11 +6,7 @@ UISelectMap::UISelectMap(std::vector<std::string> &parameterList) : UICommand ()
 	this->mapName = parameterList[1];
 }
 
-UISelectMap::UISelectMap(const std::string& xml):UICommand (){
 
-	// construye el objeto a partir del Xml recibido
-	hydrate(xml);
-}
 UISelectMap::UISelectMap() {
 
 }
@@ -21,30 +17,6 @@ UISelectMap::~UISelectMap(){
 std::string UISelectMap::serialize(){
 	return "<?xml version=\"1.0\"?><UISelectMap>"+serializeCommon(0,0)+"<map>"+mapName+"</map></UISelectMap>";
   
-}
-
-void* UISelectMap::hydrate(const std::string & xml){
-	xmlChar* field;
-	xmlXPathObjectPtr objetoXPath;
-	xmlNodeSetPtr setNodo;
-	xmlNodePtr nodo;
-	xmlDocPtr document = hydrateCommon(xml);
-	
-
-	xmlXPathContextPtr contextoXPath = xmlXPathNewContext(document);
-
-	objetoXPath = xmlXPathEvalExpression(BAD_CAST "//*/map", contextoXPath);
-	setNodo = objetoXPath->nodesetval;
-	nodo = setNodo->nodeTab[0];
-	field = xmlNodeGetContent(nodo);
-	this->mapName.assign( (char*) field );
-
-	xmlFree(field);
-	xmlXPathFreeObject(objetoXPath);
-	xmlXPathFreeContext(contextoXPath);
-	xmlFreeDoc(document);
-	xmlCleanupParser();
-	return NULL;
 }
 
 bool UISelectMap::validate(ReferenceCountPtr<GameManager>& gameManager){
