2093f15bf674ba1a54bb97b285df0b777d3e0594
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-07-02 15:37:04 +0000

    Commit de server proxy.

--- nuevo-source-experimental/source/common/net/serverproxy.cpp
+++ nuevo-source-experimental/source/common/net/serverproxy.cpp
@@ -1,6 +1,6 @@
 
 #include "serverproxy.h"
-#include "../common/model/gamemanager.h"
+#include "../../common/model/gamemanager.h"
 #include<sstream>
 #include <iostream>
 void * ServerProxy::run() {
@@ -16,12 +16,12 @@ void * ServerProxy::run() {
 
 		std::cerr<< std::endl << "################################"<< std::endl<<"ServerProxy leyendo encabezado " << std::endl;
 		// deshardcodear este 32
-		msg << socket->full_read(32);
+		msg << getSocket()->full_read(32);
 		msg >> msgLen;
 		msg >> commandName;
 
 		std::cerr << "longitud " << msgLen << " nombre " << commandName << std::endl;
-		commandXml = socket->full_read(msgLen);
+		commandXml = getSocket()->full_read(msgLen);
 		
 		std::cerr << "serializacion " << commandXml << std::endl;
 		gameManager->execute(commandName, commandXml);
@@ -31,7 +31,7 @@ void * ServerProxy::run() {
 }
 
 
-ServerProxy::ServerProxy(Socket * socket,  ReferenceCountPtr< GameManager> & gameManager):Proxy(socket,gameManager){
+ServerProxy::ServerProxy(const ReferenceCountPtr<Socket>& socket,  const ReferenceCountPtr< GameManager> & gameManager):Proxy(socket), gameManager(gameManager) {
 	
 }
 
