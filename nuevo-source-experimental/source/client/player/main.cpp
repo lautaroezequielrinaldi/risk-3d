31ba9f446d29b7969d72bd2c0123e832f5f23bf9
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-07-02 20:01:48 +0000

    Modifiquee el main para que corra a la presala

--- nuevo-source-experimental/source/client/player/main.cpp
+++ nuevo-source-experimental/source/client/player/main.cpp
@@ -1,6 +1,17 @@
 #include "client3d.h"
+#include "../../common/model/randomdice.h"
+#include<gtkmm/main.h>
+#include "pregamewindow.h"
+
 
 int main(int argc, char** argv) {
+    Gtk::Main main(argc, argv);
+    RandomDice dice;
+    ReferenceCountPtr<Game> game( new Game(new Map(),dice));
+    PreGameWindow window(game);
+
+    Gtk::Main::run(window);
+
     Client3d client;
 
     client.run();
