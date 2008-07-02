eedb472500f7af85c4f831cdac0f067f6f31c9cc
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-07-02 19:59:11 +0000

    Agregue presala.

---
+++ nuevo-source-experimental/source/client/player/pregamewindow.cpp
@@ -0,0 +1,158 @@
+#include "pregamewindow.h"
+#include "../../common/commands/chat.h"
+#include "../../common/commands/quit.h"
+#include<iostream>
+
+PreGameWindow::PreGameWindow(ReferenceCountPtr<Game>& game):
+    CommandObserver(),
+    game(game),
+	Gtk::Window(),
+	verticalBox(),
+	horizontalBox(),
+	messageTextView(),
+	messageEntry(),
+	sendMessageButton(),
+	readyToPlayButton(),
+	connectionDialogButton(),
+	quitButton(),
+	connected(false),
+	hasQuit(false) {
+    // Establece el titulo de la ventana
+	this->set_title("Pre sala de juego Risk3d");
+	// Establece el label del boton send message.
+	sendMessageButton.set_label("Send Message");
+	sendMessageButton.set_sensitive(false);
+	// Establece el label del boton ready to play.
+	readyToPlayButton.set_label("Ready To Play");
+	readyToPlayButton.set_sensitive(false);
+	// Establece el label del boton de dialogo de conexion.
+	connectionDialogButton.set_label("Connect to server...");
+	// Establece el label del boton de salir.
+	quitButton.set_label("Quit");
+
+	// Agrega el entry de mensajes al horizontal box.
+	horizontalBox.pack_start(messageEntry, Gtk::PACK_SHRINK);
+	// Agrega el button de enviar mensajes al horizontal box.
+	horizontalBox.pack_start(sendMessageButton, Gtk::PACK_SHRINK);
+	// Agrega el button ready to play al horizontal box.
+	horizontalBox.pack_start(readyToPlayButton, Gtk::PACK_SHRINK);
+	// Agrega el boton de dialogo de conexion al horizontal box.
+	horizontalBox.pack_start(connectionDialogButton, Gtk::PACK_SHRINK);
+	// Agrega el boton de quit al horizontal box.
+	horizontalBox.pack_start(quitButton, Gtk::PACK_SHRINK);
+
+	// Agrega el text view de mensajes al vertical box.
+	verticalBox.pack_start(messageTextView, Gtk::PACK_SHRINK);
+	// Agrega el horizontal box al vertical box.
+	verticalBox.pack_start(horizontalBox, Gtk::PACK_SHRINK);
+	// Agrega el vertical layout a la ventana.
+	add(verticalBox);
+	// Conecta el signal_clicked del boton connnectionDialogButton con su manejador.
+	connectionDialogButton.signal_clicked().connect(sigc::mem_fun(*this,
+		&PreGameWindow::onConnectionDialogButtonClicked));
+	// Conecta el signal_clicked del boton readyToPlayButton con su manejador.
+	sendMessageButton.signal_clicked().connect(sigc::mem_fun(*this,
+			&PreGameWindow::onSendMessageButtonClicked));
+	// Conecta el signal_clicked del boton readyToPlayButton con su manejador.
+	readyToPlayButton.signal_clicked().connect(sigc::mem_fun(*this,
+		&PreGameWindow::onReadyToPlayButtonClicked));
+	// Conecta el signal_clicked del boton quit con su manejador.
+	quitButton.signal_clicked().connect(sigc::mem_fun(*this,
+			&PreGameWindow::onQuitButtonClicked));
+	// Muestro todos los widgets.
+	show_all();
+}
+
+void PreGameWindow::showConnectionDialog() {
+	Gtk::Label serverUrlLabel("Url del servidor:");
+	Gtk::Entry serverUrlEntry;
+	serverUrlEntry.set_text("localhost");
+	Gtk::Dialog connectionDialog("Conectar al servidor");
+	connectionDialog.get_vbox()->add(serverUrlLabel);
+	connectionDialog.get_vbox()->add(serverUrlEntry);
+	connectionDialog.add_button(Gtk::Stock::OK, Gtk::RESPONSE_OK);
+	connectionDialog.add_button(Gtk::Stock::CANCEL, Gtk::RESPONSE_OK);
+	connectionDialog.show_all();
+	int result = connectionDialog.run();
+	if (result == Gtk::RESPONSE_OK) {
+		try {
+			std::cout << "Se va a transformar en string" << std::endl;
+			std::string url = serverUrlEntry.get_text();
+			std::cout << "Se llego a tranformar en string" << std::endl;
+			Socket* socket = new Socket(url, 2000);
+			std::cout << "Se va a crear socket" << std::endl;
+
+			serverProxy = new ServerProxy(socket, game);
+			JoinGame* cmd = new JoinGame();
+			serverProxy->notify(*cmd);
+			delete cmd;
+			sendMessageButton.set_sensitive(true);
+			readyToPlayButton.set_sensitive(true);
+        		connectionDialogButton.set_sensitive(false);
+            serverProxy->registerCommandObserver(this);
+			serverProxy->start();
+			connected = true;
+		} catch (SocketConnectionException& exception) {
+			Gtk::MessageDialog errorDialog(*this, "No se pudo conectar al servidor!!!", false,
+				Gtk::MESSAGE_ERROR);
+			errorDialog.set_title("Error");
+			errorDialog.run();
+		}
+	}
+}
+
+void PreGameWindow::onConnectionDialogButtonClicked() {
+	this->showConnectionDialog();
+}
+
+void PreGameWindow::onSendMessageButtonClicked() {
+	if (connected) {
+		Chat* cmd = new Chat();
+		std::string message = messageEntry.get_text();
+		cmd->setMainMsg(message);
+		serverProxy->notify(*cmd);
+        delete cmd;
+		messageEntry.set_text("");
+	}	
+}
+
+void PreGameWindow::onReadyToPlayButtonClicked() {
+	ReadyToPlay* cmd = new ReadyToPlay();
+	serverProxy->notify(*cmd);
+	delete cmd;
+	readyToPlayButton.set_sensitive(false);
+}
+
+void PreGameWindow::onQuitButtonClicked() {
+	if (connected) {
+		Quit* cmd = new Quit();
+	    serverProxy->notify(*cmd);	
+		delete(cmd);
+		serverProxy->cancel();
+	}
+	hasQuit = true;
+	Gtk::Main::quit();
+}
+
+bool PreGameWindow::userHasQuit() {
+	return hasQuit;
+}
+
+void PreGameWindow::commandExecuted(SelectMap& cmd) {
+	
+}
+
+ReferenceCountPtr<ServerProxy> PreGameWindow::getServerProxy() {
+    return serverProxy;
+}
+
+void PreGameWindow::commandExecuted(Chat& cmd) {
+	std::string buffer = messageTextView.get_buffer()->get_text();
+	buffer =  buffer + "\r\n" + cmd.getMainMsg();
+	messageTextView.get_buffer()->set_text(buffer);
+}
+
+PreGameWindow::~PreGameWindow() {
+	// No realiza ninguna accion.
+}
+
---
+++ nuevo-source-experimental/source/client/player/pregamewindow.h
@@ -0,0 +1,144 @@
+#ifndef __PREGAMEWINDOW_H__
+#define __PREGAMEWINDOW_H__
+
+#include<gtkmm/main.h>
+#include<gtkmm/window.h>
+#include<gtkmm/box.h>
+#include<gtkmm/button.h>
+#include<gtkmm/label.h>
+#include<gtkmm/textview.h>
+#include<gtkmm/entry.h>
+#include<gtkmm/stock.h>
+#include<gtkmm/messagedialog.h>
+#include<gtkmm/dialog.h>
+#include "../../common/commands/joingame.h"
+#include "../../common/commands/readytoplay.h"
+#include "../../common/commands/quit.h"
+#include "../../common/commands/commandobserver.h"
+#include "net/serverproxy.h"
+#include "../../common/smartpointer/referencecountptr.h"
+
+/**
+ * Representa la presala del juego donde puede haber chat y seleccion de mapa.
+ */
+class PreGameWindow: public CommandObserver, public Gtk::Window {
+	/**
+	 * Atributos privados de la clase PreGameWindow.
+	 */
+	private:
+		/**
+		 * Almacena la referencia al server proxy a conectarse.
+		 */
+		ReferenceCountPtr<ServerProxy> serverProxy;
+        /**
+         * Almacena una referencia al game.
+         */
+        ReferenceCountPtr<Game>& game;
+		/**
+		 * Almacena el layout vertical de la ventana.
+		 */
+		Gtk::VBox verticalBox;
+		/**
+		 * Almacena el layout horizontal de la ventana.
+		 */
+		Gtk::HBox horizontalBox;
+		/**
+		 * Almacena el textview para mostrar los mensajes de chat.
+		 */
+		Gtk::TextView messageTextView;
+		/**
+		 * Almacena el entry para ingresar los mensajes de chat.
+		 */
+		Gtk::Entry messageEntry;
+		/**
+		 * Almacena el boton para enviar mensajes de chat.
+		 */
+		Gtk::Button sendMessageButton;
+		/**
+		 * Almacena el boton para enviar ready to play.
+		 */
+		Gtk::Button readyToPlayButton;
+		/**
+		 * Almacena una referencia al boton de dialogo de conexion.
+		 */
+		Gtk::Button connectionDialogButton;
+		/**
+		 * Almacena una referencia al boton quit para salir.
+		 */
+		Gtk::Button quitButton;
+		/**
+		 * Almacena si se ha conectado.
+		 */
+		bool connected;
+		/**
+			* Almacena si se ha salido.
+			*/
+		bool hasQuit;
+	/**
+	 * Metodos privados de la clase PreGameWindow.
+	 */
+	private:
+		/**
+		 * Constructor copia de la clase PreGameWindow.
+		 */
+		PreGameWindow(const PreGameWindow& otherInstance);
+		/**
+		 * Operador de asignacion de la clase PreGameWindow.
+		 */
+		PreGameWindow& operator=(const PreGameWindow& otherInstance);
+	/**
+	 * Metodos protegidos de la clase PreGameWindow.
+	 */
+	protected:
+		/**
+		 * Muestra el cuadro de dialogo de conexion con el servidor.
+		 */
+		void showConnectionDialog();
+		/**
+		 * Manejador de la senial signal_clicked del boton connection dialog button.
+		 */
+		void onConnectionDialogButtonClicked();
+		/**
+		 * Manejador de la senial signal_clicked del boton send messsage button.
+		 */
+		void onSendMessageButtonClicked();
+		/**
+		 * Manejador de la senial signal_clicked del boton ready to play button.
+		 */
+		void onReadyToPlayButtonClicked();
+		/**
+		 * Manejador de la senial signal_clicked del boton quit.
+		 */
+		void onQuitButtonClicked();
+	/**
+	 * Metodos publicos de la clase PreGameWindow.
+	 */
+	public:
+		/**
+		 * Constructor de la clase PreGameWindow.
+		 */
+		PreGameWindow(ReferenceCountPtr<Game>& game);
+		/**
+		 * Obtiene el game manager usado durante la presala.
+		 */
+		ReferenceCountPtr<ServerProxy> getServerProxy();
+		/**
+		 * Devuelve si ha salido el jugador presionando el boton quit.
+		 */
+		bool userHasQuit();
+		/**
+		 * Responde ante la llegada de un commando mapList.
+		 */
+		virtual void commandExecuted(SelectMap& cmd);
+		/**
+		 * Responde ante la ll egada de un comando chat.
+		 */
+		virtual void commandExecuted(Chat& cmd);
+		/**
+		 * Destructor virtual de la clase PreGameWindow.
+		 */
+		virtual ~PreGameWindow();
+};
+
+#endif /** __PREGAMEWINDOW_H__ */
+
