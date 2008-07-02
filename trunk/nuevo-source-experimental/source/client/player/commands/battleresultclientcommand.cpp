6a7a09879a02e18d4dc662c8357ef20003e75ad2
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-07-02 07:58:31 +0000

    Modifique BattleResult

--- trunk/nuevo-source-experimental/source/client/player/commands/battleresultclientcommand.cpp
+++ trunk/nuevo-source-experimental/source/client/player/commands/battleresultclientcommand.cpp
@@ -5,9 +5,31 @@ BattleResultClientCommand::BattleResultClientCommand(ReferenceCountPtr<Game>& ga
 }
 
 void BattleResultClientCommand::execute() {
-    ReferenceCountPtr<Mapa> map = this->game->getMapa();
-    ReferenceCountPtr<Pais> pais = map->obtenerPais(this->paisDestino);
-    pais->addArmies(getArmyCount());
+    ReferenceCountPtr<Game> game = this->gameManager->getGame();
+    ReferenceCountPtr<Mapa> map = game->getMapa();
+    ReferenceCountPtr<Pais> paisAtacante = map->obtenerPais( command.getAttackerLand() );
+    ReferenceCountPtr<Pais> paisDefensor = map->obtenerPais( command.getDefenderLand() );
+
+    ReferenceCountPtr<Player> playerAtacante = game->getPlayer( command.getAttackercolor() );
+    ReferenceCountPtr<Player> playerDefensor = game->getPlayer( command.getDefenderColor() );
+
+    paisAtacante->removeArmies(command.getAttackerResult());
+    paisDefensor->removeArmies(command.getDefenderResult());
+
+    if ( paisDefensor->getArmyCount() == 0  ){
+        std::string defLand = command.getDefenderLand();
+        playerDefensor->removeConqueredLand(defLand);
+
+        playerAtacante->addConqueredLand(defLand );
+        paisAtacante->removeArmies( command.getConquest() );
+        paisDefensor->addArmies(  command.getConquest() );
+        std::vector<std::string> vecContinentes = game->conformContinent(playerAtacante->getConqueredLandList() );
+        for ( unsigned int i=0; i<vecContinentes.size();i++){
+            //si playerAtacante no es dueño todavia de uno de los continentes conquistados segun vecContinentes
+            if ( !playerAtacante->continentOwner( vecContinentes[i] ) )
+                //seteo al continente actual como conquistado por el player atacante
+                playerAtacante->addConqueredContinent(vecContinentes[i] );
+        }
 }
 
 std::string BattleResultClientCommand::getType() {
