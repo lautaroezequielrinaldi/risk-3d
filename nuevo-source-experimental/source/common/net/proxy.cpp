ec1d3dcdf21a2bac6dbbc82b970c181edf310b37
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-07-02 15:56:00 +0000

    Cambie y fixie algunos errores.

--- nuevo-source-experimental/source/common/net/proxy.cpp
+++ nuevo-source-experimental/source/common/net/proxy.cpp
@@ -1,5 +1,6 @@
 
 #include "proxy.h"
+#include "../../common/commands/command.h"
 #include <sstream>
 #include <iostream>
 
@@ -10,6 +11,18 @@ ReferenceCountPtr<Socket> Proxy::getSocket() {
     return socket;
 }
 
+void Proxy::notify(Command& command) {
+    std::string header;
+    std::string message;
+
+    message = command.serialize();
+    
+    header = message.size() + " " + command.getName();
+
+    getSocket()->write(header.c_str(), header.size());
+    getSocket()->write(message.c_str(), message.size());
+}
+    
 Proxy::~Proxy(){
 }
 
