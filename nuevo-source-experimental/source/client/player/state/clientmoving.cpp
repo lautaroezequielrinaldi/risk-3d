d5abfd64168cd32895cc152c1953fa19ef8082f6
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-07-08 05:00:40 +0000

    Agregue estado moviendo.

---
+++ nuevo-source-experimental/source/client/player/state/clientmoving.cpp
@@ -0,0 +1,41 @@
+#include "clientmoving.h"
+#include "../gamewindow.h"
+
+ClientMoving::ClientMoving(GameWindow& gameWindow): ClientState(gameWindow) {
+    // No realiza ninguna accion.
+}
+
+ClientMoving::~ClientMoving() {
+    // No realiza ninguna accion.
+}
+
+void ClientMoving::selectCountry(ReferenceCountPtr<Pais>& country) {
+    if (country != NULL) {
+        if (getFirstCountry() == NULL) {
+            setFirstCountry(country);
+            ClientState::selectCountry(country);
+        } else if (getFirstCountry()->getNombre() == country->getNombre()) {
+            ClientState::selectCountry(country);
+        } else {
+            if (getGameWindow().getServerProxy() != NULL) {
+                setSecondCountry(country);
+                std::vector<std::string> parameters;
+                std::ostringstream armyCountStr;
+                std::ostringstream firstPlayerStr;
+
+                armyCountStr << getArmyCount();
+                firstPlayerStr << getGameWindow().getServerProxy()->getMe();
+            
+                parameters.push_back(getFirstCountry()->getNombre());
+                parameters.push_back(getSecondCountry()->getNombre());
+                parameters.push_back(armyCountStr.str());
+                parameters.push_back(firstPlayerStr.str());
+
+                Move move(parameters);
+                move.setFrom(getGameWindow().getServerProxy()->getMe());
+                getGameWindow().getServerProxy()->notify(move);
+            }
+        }
+    }       
+}
+
