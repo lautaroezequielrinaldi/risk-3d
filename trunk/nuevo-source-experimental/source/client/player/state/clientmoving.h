548db0a445ca6e31797e6d79e9720f8201261b2e
pmviva <pmviva@1d5d0233-af4c-0410-b063-6b6c16de9a5a>
2008-07-08 05:00:40 +0000

    Agregue estado moviendo.

---
+++ trunk/nuevo-source-experimental/source/client/player/state/clientmoving.h
@@ -0,0 +1,31 @@
+#ifndef __CLIENTMOVING_H__
+#define __CLIENTMOVING_H__
+
+#include "clientstate.h"
+
+class ClientMoving: public ClientState {
+    /**
+     * Atributos privados de la clase ClientMoving.
+     */
+    private:
+
+    /**
+     * Metodos publicos de la clase ClientMoving.
+     */
+    public:
+        /**
+         * Constructor de la clase ClientMoving.
+         */
+        ClientMoving(GameWindow& gameWindow);
+        /**
+         * Procesa seleccion de un pais.
+         */
+        virtual void selectCountry(ReferenceCountPtr<Pais>& country);
+        /**
+         * Destructor de la clase ClientMoving.
+         */
+        virtual ~ClientMoving();
+};
+
+#endif /** __CLIENTMOVING_H__ */
+
